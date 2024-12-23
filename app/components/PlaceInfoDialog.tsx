b5ad6e8d1f1a12958e700fd9c4c6ffbb7fa9509f
100JM <bjm_9310@naver.com>
2024-12-23 17:50:05 +0900

    장소 상세 정보 다이얼로그 기본디자인

--- app/components/PlaceInfoDialog.tsx
+++ app/components/PlaceInfoDialog.tsx
@@ -1,13 +1,14 @@
+import { useRef } from 'react';
 import { Map, MapMarker, useKakaoLoader } from 'react-kakao-maps-sdk'
 
-import Link from 'next/link';
 import useDialog from '../store/useDialog';
 import usePlaceData from '../store/usePlaceData';
 
 import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
-// import DialogActions from '@mui/material/DialogActions';
+import DialogActions from '@mui/material/DialogActions';
+import Rating from '@mui/material/Rating';
 
 const PlaceInfoDialog = () => {
     const { loading, error } = useKakaoLoader({
@@ -21,20 +22,27 @@ const PlaceInfoDialog = () => {
     const { showPlaceInfo, setShowPlaceInfo } = useDialog();
     const { selectedPlace, resetSelectedPlace } = usePlaceData();
 
+    const fileInputRef = useRef<HTMLInputElement>(null);
+
     const handleClosePlaceInfoDialog = () => {
         setShowPlaceInfo(false)
         resetSelectedPlace();
+    };
+
+    const handleClickFileInput = () => {
+        ((fileInputRef.current) as HTMLInputElement).click();
     };
 
     return (
         <Dialog
             open={showPlaceInfo}
             onClose={() => setShowPlaceInfo(false)}
-            maxWidth="md"
+            maxWidth="lg"
             fullWidth={true}
         >
             <DialogTitle className="flex justify-between items-center border-b">
                 <div>
+                    <p className="text-xs">{selectedPlace.category_name}</p>
                     <p className="text-2xl text-[#2391ff]">{selectedPlace.place_name}</p>
                     <p className="text-sm text-[#868e96]">{selectedPlace.address_name}<i className="ri-file-copy-2-line"></i></p>
                 </div>
@@ -45,11 +53,20 @@ const PlaceInfoDialog = () => {
                 </div>
             </DialogTitle>
             <DialogContent className="border-b">
-                <div className="place-info pt-5 flex justify-center">
-                    <div className="w-full lg:w-1/2 lg:mr-2">
+                <div className="mt-5">
+                    <div>
+                        <input type="file" accept="image/*" capture="environment" className="hidden" ref={fileInputRef} />
+                        <button className="w-full h-40 bg-gray-200 rounded-md text-gray-500" onClick={handleClickFileInput}>
+                            <p>장소와 연관된 사진을 업로드할 수 있어요.</p>
+                            <i className="ri-image-add-fill"></i>
+                        </button>
+                    </div>
+                </div>
+                <div className="place-info mt-5 flex justify-center">
+                    <div className="w-full mb-6 lg:w-1/2 lg:mr-2 lg:mb-0">
                         <Map
                             center={{ lat: Number(selectedPlace.y), lng: Number(selectedPlace.x) }}
-                            className="w-full h-40 rounded-md shadow-lg shadow-gray-300"
+                            className="w-full h-80 rounded-md shadow-lg shadow-gray-300"
                             level={2}
                             zoomable={false}
                             draggable={false}
@@ -57,17 +74,30 @@ const PlaceInfoDialog = () => {
                             <MapMarker position={{ lat: Number(selectedPlace.y), lng: Number(selectedPlace.x) }} />
                         </Map>
                     </div>
-                    <div className="w-full lg:w-1/2 lg:ml-2">
-                        <p className="mb-2 text-sm">
-                            <span>{selectedPlace.category_name}</span>
-                        </p>
-                        <Link href={selectedPlace.place_url} target="_blank" className="p-2 bg-[#2391ff] rounded-md text-white">
-                            <span>카카오 지도에서 자세히 보기</span>
-                            <i className="ri-external-link-line"></i>
-                        </Link>
+                    <div className="w-full overflow-y-auto h-80 lg:w-1/2 lg:ml-2">
+                        <div>
+                            <h3 className="text-gray-500">장소에 대한 의견을 남겨주세요.</h3>
+                            <Rating name="size-medium" defaultValue={5} />
+                            <div>
+                                <input type="text" className="border-b focus:outline-none w-full" />
+                            </div>
+                        </div>
+                        <div className="h-[calc(100%-80px)] flex justify-center items-center">
+                            <span>기록된 리뷰가 없습니다🥲</span>
+                        </div>
                     </div>
                 </div>
             </DialogContent>
+            <DialogActions>
+                <a href={selectedPlace.place_url} target="_blank" className="bg-[#2391ff] rounded-md text-white p-2 w-1/2 mr-1 text-center">
+                    <span>카카오 지도에서 보기</span>
+                    <i className="ri-external-link-line ml-2"></i>
+                </a>
+                <a href={`https://map.kakao.com/link/roadview/${selectedPlace.id}`} target="_blank" className="bg-[#2391ff] rounded-md text-white p-2 w-1/2 ml-1 text-center">
+                    <span>로드뷰 바로가기</span>
+                    <i className="ri-road-map-line ml-2"></i>
+                </a>
+            </DialogActions>
         </Dialog>
     )
 };
