0829e2202bc17a61ee3e65164086676c7aa47f19
100JM <bjm_9310@naver.com>
2024-12-24 17:57:03 +0900

    update

--- app/components/CategoryButton.tsx
+++ app/components/CategoryButton.tsx
@@ -1,11 +1,37 @@
+import axios from "axios";
+
+import usePlaceData from "../store/usePlaceData";
+import useMapData from "../store/useMapData";
 import { categoryList } from "../utils/categoryList";
 
 const CategoryButton = () => {
+    const { setCategoryPlaceList, resetSelectedPlaceRef } = usePlaceData();
+    const { mapCenter } = useMapData();
+
+    const fetchCategory = async (code: string) => {
+        resetSelectedPlaceRef();
+
+        try {
+            if (mapCenter) {
+                const categoryResponse = await axios.get(`api/kakao-category-api?x=${mapCenter.lng}&y=${mapCenter.lat}&&category_group_code=${code}`);
+                setCategoryPlaceList(categoryResponse.data);
+            }
+        } catch (error) {
+            console.log('fetchCategory Error:', error);
+        }
+    };
+
+    const handleClickCategoryBtn = (keyword: string) => {
+        if (keyword !== '인기 장소' && keyword !== '맛집') {
+            fetchCategory(keyword);
+        }
+    };
+
     return (
         <>
             {categoryList.map((c) => {
                 return (
-                    <button key={c.icon} className="py-1 px-2 shadow-md shadow-gray-400 border rounded-3xl group">
+                    <button key={c.icon} className="py-1 px-2 shadow-md shadow-gray-400 border rounded-3xl group" onClick={() => handleClickCategoryBtn(c.code)}>
                         <i className={`${c.icon} ${c.color} p-0`}></i>
                         <span className="group-hover:text-[#2391ff] ml-1 text-sm">{c.text}</span>
                     </button>
--- app/utils/categoryList.ts
+++ app/utils/categoryList.ts
@@ -2,41 +2,49 @@ export const categoryList = [
     {
         text: '인기 장소',
         icon: 'ri-star-fill',
-        color: 'text-orange-400'
+        color: 'text-orange-400',
+        code: '인기 장소'
     },
     {
         text: '맛집',
         icon: 'ri-restaurant-2-fill',
-        color: 'text-orange-400'
+        color: 'text-orange-400',
+        code: '맛집'
     },
     {
         text: '음식점',
         icon: 'ri-restaurant-fill',
-        color: 'text-green-400'
+        color: 'text-green-400',
+        code: 'FD6'
     },
     {
         text: '카페',
         icon: 'ri-cup-fill',
-        color: 'text-amber-800'
+        color: 'text-amber-800',
+        code: 'CE7'
     },
     {
         text: '편의점',
         icon: 'ri-store-fill',
-        color: 'text-amber-400'
+        color: 'text-amber-400',
+        code: 'CS2'
     },
     {
         text: '숙박',
         icon: 'ri-hotel-fill',
-        color: 'text-purple-400'
+        color: 'text-purple-400',
+        code: 'AD5'
     },
     {
         text: '약국',
         icon: 'ri-capsule-fill',
-        color: 'text-red-400'
+        color: 'text-red-400',
+        code: 'PM9'
     },
     {
         text: '병원',
         icon: 'ri-add-box-fill',
-        color: 'text-red-400'
+        color: 'text-red-400',
+        code: 'HP8'
     },
 ];
